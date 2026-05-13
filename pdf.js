8243e3ee1dac45274db95903ba9c43b02638f375
jamilsalim-afk <jamil.salim@gmail.com>
2026-05-13 08:52:56 -0400

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -1,1 +1,9 @@
-assss
+const coresPDF = {
+    "reserva-ensino": [212, 237, 218], // verde claro
+    "pps": [11, 61, 145], // azul escuro (texto)
+    "estudos": [255, 229, 180], // laranja claro
+    "reuniao": [208, 235, 255], // azul claro
+    "caed": [255, 243, 205], // amarelo
+    "reposicao": [30, 126, 52], // verde escuro
+    "marcacao-extra": [224, 224, 224]
+};
