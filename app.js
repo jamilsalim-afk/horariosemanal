b113b129d9e26709755137e7ac7bca608745af8b
jamilsalim-afk <jamil.salim@gmail.com>
2026-05-13 08:34:40 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const SHEETS={
+INTEGRADO:{id:'1j33kiPqwtzZNuvkBgYDaIiXZvVMY_J0qWAtRfYGdnD8',gid:'1357770092'},
+SUPERIOR:{id:'14ALXZgFIT68ee9ajuIdG63SpGVm0HyTjwp63-J6vRyg',gid:'669887707'}
+};
+
+let dadosGlobais=[],turmasDaPlanilha=[],semanasAgrupadas={};
+
