c7c4df5363b67ffe9d6f999ff9376c43da0ee8c3
jamilsalim-afk <jamil.salim@gmail.com>
2026-05-13 09:14:59 -0400

    Update turmas.js

--- js/turmas.js
+++ js/turmas.js
@@ -1,1 +1,29 @@
-fasdfas
+  function gerarFichaTurma(nomeTurma){
+
+  const sem = document.getElementById('selectSemana').value;
+  const dias = semanasAgrupadas[sem].dias;
+
+  let resultado = [];
+
+  Object.keys(dias).forEach(dia => {
+
+    dias[dia].forEach(r => {
+
+      const horario = r[1];
+      const idx = dadosGlobais[0].indexOf(nomeTurma);
+      const val = (r[idx] || "");
+
+      if(val){
+        resultado.push({
+          dia,
+          horario,
+          aula: val
+        });
+      }
+
+    });
+
+  });
+
+  return resultado;
+}
