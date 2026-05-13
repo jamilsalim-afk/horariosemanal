56d7ed8bbd94e0c6a8d409d46e8f3b4fc6c6b0c0
jamilsalim-afk <jamil.salim@gmail.com>
2026-05-13 08:36:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,53 @@ SUPERIOR:{id:'14ALXZgFIT68ee9ajuIdG63SpGVm0HyTjwp63-J6vRyg',gid:'669887707'}
 
 let dadosGlobais=[],turmasDaPlanilha=[],semanasAgrupadas={};
 
+  function isSemanaPassada(dataStr){
+  const [d,m,a] = dataStr.split('/');
+  const data = new Date(a, m-1, d);
+
+  const hoje = new Date();
+
+  // pega segunda-feira da semana atual
+  const dia = hoje.getDay();
+  const diff = hoje.getDate() - dia + (dia === 0 ? -6 : 1);
+  const segundaAtual = new Date(hoje.setDate(diff));
+
+  return data < segundaAtual;
+}
+
+function limparPainelAlteracoes(){
+  document.getElementById("conteudoAlteracoes").innerText = "";
+  document.getElementById("painelAlteracoes").style.display = "none";
+}
+
+function mostrarPainelVagas(texto){
+  document.getElementById("conteudoVagas").innerText = texto;
+
+  document.querySelector("#painelVagas strong").innerText = "📅 Aulas Vagas";
+
+  document.getElementById("painelVagas").style.display = "block";
+}
+
+function fecharPainelVagas(){
+  document.getElementById("painelVagas").style.display = "none";
+}
+
+function copiarVagas(){
+  const texto = document.getElementById("conteudoVagas").innerText;
+  navigator.clipboard.writeText(texto).then(()=>{
+    alert("Copiado!");
+  });
+}
+  
+function trocarModalidade(){
+  window.trocouModalidade = true;
+
+  limparPainelAlteracoes(); // 🔥 ESSENCIAL
+
+  init();
+}
+
+  function trocarSemana(){
+  limparPainelAlteracoes(); // 🔥 ESSENCIAL
+  renderizarTabela();
+}
