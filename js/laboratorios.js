c5f2639999f12fd074852d67e6075c1ffc1f40f5
jamilsalim-afk <jamil.salim@gmail.com>
2026-07-10 12:13:25 -0400

    Update laboratorios.js

--- js/laboratorios.js
+++ js/laboratorios.js
@@ -1196,7 +1196,7 @@ ${a.turma}`
         1:{cellWidth:22}
     };
     labs.forEach((lab, i)=>{
-        columnStyles[i+2]={cellWidth:55};
+        columnStyles[i+2]={cellWidth:50};
     });
     // =====================================
     // TABELA
